ba560d1bb4a0373486b2ffa8bdac40c91c401ac1
Aryan Singh Chandel <shiroonigami23@gmail.com>
2025-09-19 11:49:03 +0530

    Remove local sync script from repo

--- sync_assignments.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-cp ~/class-solutions/Assignment/* ~/class-solutions/
-git add .
-git commit -m "Auto-sync Assignment files"
-git pull --rebase
-git push origin main
-
